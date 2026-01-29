034392097c7587b6e61597c76ddb9d6175e6fcd5
dhingradeep51 <dhingradeepak51@gmail.com>
2026-01-29 19:10:36 +0530

    resetting build path

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './context/cart';
 import { AuthProvider } from './context/auth'; // Import your new provider
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
