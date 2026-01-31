898a5a0e4b8e983a1b441db6ef49fe934293a34a
dhingradeep51 <dhingradeepak51@gmail.com>
2026-01-31 16:51:13 +0530

    resetting build path

--- frontend/src/pages/User/Dashboard.jsx
+++ frontend/src/pages/User/Dashboard.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Layout from "../../components/Layout";
-import UserMenu from "../../components/Menus/UserMenu";
 import { useAuth } from "../../context/auth";
 
 const Dashboard = () => {
@@ -12,7 +11,6 @@ const Dashboard = () => {
       <div className="container-flui m-3 p-3">
         <div className="row">
           <div className="col-md-3">
-            <UserMenu />
           </div>
           <div className="col-md-9">
             <div className="card w-75 p-3 shadow border-0">
