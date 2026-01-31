f0934c798de74c68ab5b5fd9ce9cfc0e40d99a30
dhingradeep51 <dhingradeepak51@gmail.com>
2026-01-31 17:21:55 +0530

    resetting build path

--- Backend/Controllers/orderController.js
+++ Backend/Controllers/orderController.js
@@ -3,7 +3,7 @@ import moment from "moment";
 import ProductModel from "../Models/productModel.js";
 import userModel from "../Models/userModel.js";
 // ✅ Ensure the import path and filename match your utils folder exactly
-import { sendNotification } from "../utils/notificationUtil.js";
+import { sendNotification } from "../utils/notificationUtils.js";
 
 // --- PLACE NEW ORDER ---
 export const placeOrderController = async (req, res) => {
