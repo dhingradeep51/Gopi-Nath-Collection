955db9db9bf3884b455bff9a921f9718b31f99a0
dhingradeep51 <dhingradeepak51@gmail.com>
2026-02-02 11:52:54 +0530

    resetting build path

--- Backend/Routes/paymentRoute.js
+++ Backend/Routes/paymentRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { initiatePayment, checkStatus } from "../Controllers/paymentController.js";
-import { requireSignIn } from "../Middlewares/authMiddleware.js"; // Case-sensitive!
+import { requireSignIn } from "../Middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/initiate", requireSignIn, initiatePayment);
 router.post("/status/:merchantTransactionId", checkStatus);
+router.get("/status/:merchantTransactionId", checkStatus);
 
 export default router;
