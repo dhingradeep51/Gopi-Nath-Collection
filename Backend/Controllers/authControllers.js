ac75065ebc3933a9dd14f412b9514b7168d8ad1d
dhingradeep51 <dhingradeepak51@gmail.com>
2026-02-05 22:29:56 +0530

    resetting build path

--- Backend/Controllers/authControllers.js
+++ Backend/Controllers/authControllers.js
@@ -20,6 +20,7 @@ export const sendOTPController = async (req, res) => {
     const transporter = nodemailer.createTransport({
       host: "smtp-relay.brevo.com",
       port: 2525,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
